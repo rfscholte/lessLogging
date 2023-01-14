1846c3f9a99dcff933726a90951ab33923791feb
rfscholte <rfscholte@apache.org>
2023-01-14 11:32:50 +0100

    Simplify code, fix fake placeholder

--- spring-boot_jpa/src/test/java/com/sourcegrounds/lesslogging/repository/LoremRepositoryTest.java
+++ spring-boot_jpa/src/test/java/com/sourcegrounds/lesslogging/repository/LoremRepositoryTest.java
@@ -20,18 +20,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
 class LoremRepositoryTest
 {
     @Autowired
     private LoremRepository repository;
 
     @Test
-    void blaat()
+    void count()
     {
         assertEquals( 0, repository.count() );
     }
